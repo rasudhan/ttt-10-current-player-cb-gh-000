90bf7bf91fd139d8c05a91d4ff7dd368f24b8e93
Anandha Sudhan <rasudhan@gmail.com>
2018-10-02 23:38:10 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -5,7 +5,7 @@ end
 def turn_count(board)
   count=0
   board.each{|token| token=="X" || token=="O"? count+=1:count+=0 }
-  puts count
+  count
 end
 
 board = ["O", " ", " ", " ", "X", " ", " ", " ", "X"]
